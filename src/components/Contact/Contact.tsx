8c2f57c5ff2ed9279c682b58c3e702e519eed253
Fannie <113436254+fanniewallner@users.noreply.github.com>
2023-04-28 12:49:21 +0200

    fix socials

--- src/components/Contact/Contact.tsx
+++ src/components/Contact/Contact.tsx
@@ -1,14 +1,14 @@
-import Socials from "../Socials/Socials";
+
 import { FaGithub, FaLinkedin } from "react-icons/fa";
 import { BsPersonVcardFill } from "react-icons/bs";
 import './Contact.scss'
 import { useEffect, useState } from "react";
-import { Link } from "react-router-dom";
 
 const iconStyles = {color: "palevioletred", fontSize: "2.5rem"}
 
 function Contact () {
     const [show, setShow] = useState(false)
+
 
     useEffect(() => {
         setShow(true)
@@ -21,9 +21,9 @@ return (
     <div className={`contact__text ${show ? "show" : ""}`}>I am now looking for an internship where I will have the chance to develop my technical skills and gain practical experience with support and tutoring. Do you think we could be a good match? Email me at fannie.wallner@medieinstitutet.se or connect with me on LinkedIn</div>
         </div>
         <div className="socials">
-        <Socials url="https://github.com/fanniewallner" iconlink={<FaGithub style={iconStyles}/>}></Socials>
-    <Socials url="https://www.linkedin.com/in/fannie-wallner-87635b239/" iconlink={<FaLinkedin style={iconStyles}/>}></Socials>
-    <Socials url="https://www.dropbox.com/s/ptstl2tu2o5g134/Fannie%20Wallner.pdf?dl=0" iconlink={<BsPersonVcardFill style={iconStyles}/>}></Socials>
+        <a target="_blank" href="https://github.com/fanniewallner">{<FaGithub style={iconStyles}/>}</a>
+        <a target="_blank" href="https://www.linkedin.com/in/fannie-wallner-87635b239/">{<FaLinkedin style={iconStyles}/>}</a>
+        <a target="_blank" href="https://www.dropbox.com/s/ptstl2tu2o5g134/Fannie%20Wallner.pdf?dl=0">{<BsPersonVcardFill style={iconStyles}/>}</a>
         </div>
     </div>
     </>
