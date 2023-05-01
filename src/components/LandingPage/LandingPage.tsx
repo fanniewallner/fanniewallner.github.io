a16841643f077484ad1df2d13dd26e39205570cc
Fannie <113436254+fanniewallner@users.noreply.github.com>
2023-05-01 18:50:34 +0200

    (fix) grammatics

--- src/components/LandingPage/LandingPage.tsx
+++ src/components/LandingPage/LandingPage.tsx
@@ -15,8 +15,8 @@ function LandingPage () {
 return (
     <>
     <div className={`landingPage`}>
-        <h1 className={`landingPage__title ${show ? "show" : ""}`}>Hello there, my name is Fannie</h1>
-        <p className={`landingPage__text ${show ? "show" : ""}`}>I am a front end developer student at Medieinstitutet in Stockholm</p>
+        <h1 className={`landingPage__title ${show ? "show" : ""}`}>Hello there, my name is Fannie.</h1>
+        <p className={`landingPage__text ${show ? "show" : ""}`}>I am a front end developer student at Medieinstitutet in Stockholm.</p>
     </div>
     <Navigation></Navigation>
     </>
