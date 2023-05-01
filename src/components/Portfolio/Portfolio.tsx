8ff6f17e5cdd2fd7e1b43f19c734babd1f2fc57b
Fannie <113436254+fanniewallner@users.noreply.github.com>
2023-05-01 18:38:50 +0200

    (fix) adding comment to one card

--- src/components/Portfolio/Portfolio.tsx
+++ src/components/Portfolio/Portfolio.tsx
@@ -14,8 +14,8 @@ function Portfolio () {
         <>
         <h1 className={`portfolio__title ${show ? "show" : ""}`}>Portfolio</h1>
         <div className={`portfolioWrapper ${show ? "show" : ""}`} >
-        <ProjectCard title="Tic-tac-toe" img="/assets/tic-tac-toe-small.png" description="A simple Tic-Tac-Toe game built in Vue" tryLink={""} gitLink={""}></ProjectCard>
-        <ProjectCard title="To-do App" img="/assets/todo-vue.png" description="To-do app built in Vue" tryLink={""} gitLink={"https://github.com/fanniewallner/Todo.vue"}></ProjectCard>
+        <ProjectCard title="Tic-tac-toe" img="/assets/tic-tac-toe-small.png" description="A simple Tic-Tac-Toe game built in Vue. Demo and code coming soon" tryLink={""} gitLink={""}></ProjectCard>
+        <ProjectCard title="To-do App" img="/assets/todo-vue.png" description="To-do app built in Vue" tryLink={"https://todo-vueapplication.netlify.app/"} gitLink={"https://github.com/fanniewallner/Todo.vue"}></ProjectCard>
         <ProjectCard title="Geezer" img="/assets/geezer-small.png" description="A webshop group project at Medieinstitutet. Built in JS" tryLink={"https://geezershop.netlify.app/"} gitLink={"https://github.com/fanniewallner/geezer"}></ProjectCard>
         </div>
         <Navigation></Navigation>
